013569eaffd1c32d8cb77ea3f1d12b3689513a88
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2022-05-07 14:52:22 -0400

    posting correctly

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,2 +1,28 @@
 class TestsController < ApplicationController
+    skip_before_action :verify_authenticity_token
+
+  def show
+    @test = Test.find(params[:id])
+    render json: @test
+  end
+
+  def index
+    @tests = Test.all
+    render json: @tests
+  end
+
+  def create
+    @test = Test.create(string_to_cut: params[:string_to_cut])
+    render json: @test
+
+    # redirect_to action: 'tests#show', id: @test.id
+
+  end
+
+  def test_params
+    params.require(:test).permit(:string_to_cut)
+  end
+
+  def display
+  end
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,21 @@
 class Test < ApplicationRecord
+    def string_to_cut=(string_to_cut)
+        super(string_to_cut)
+        self.return_string = generate_output(string_to_cut)
+    end
+
+    private
+
+    def generate_output(string_to_cut)
+      new_output_string = string_to_cut.split("")
+      final_output_string_array = []
+      counter = 1
+      new_output_string.each do |letter|
+        if counter % 3 == 0
+          final_output_string_array.append(letter)
+        end      
+        counter = counter + 1
+      end
+      final_output_string_array.join("")
+    end
 end
